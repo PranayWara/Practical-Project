95b66fe123ce00e8dd9e15d913eb8ee1238de7ec
Pranay Wara <pranay.wara@gmail.com>
2021-08-06 14:31:34 +0000

    pw:scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,10 @@
 
 
 # copy over compose yaml to manager node
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
+scp -i ~/.ssh/ansible_id.rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 
 # docker stack deploy
-ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-manager << EOF
+ssh -i ~/.ssh/ansible_id.rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
     docker stack deploy --compose-file docker-compose.yaml draft
