53ab3faf34b9281a512d8161787b746c96f58d4f
Pranay Wara <pranay.wara@gmail.com>
2021-08-06 14:20:12 +0000

    pw:help

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/horri/Practical-Project/ansible -i ansible/inventory.yaml ansible/ansible-playbook.yaml
+/home/jenkins/Practical-Project/ansible -i ansible/inventory.yaml ansible/ansible-playbook.yaml
