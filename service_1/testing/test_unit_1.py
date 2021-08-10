9a5e864093db9c0f572cdd78c8add8305d18eeeb
Pranay Wara <pranay.wara@gmail.com>
2021-08-10 10:50:40 +0000

    pw:tests donw

--- service_1/testing/test_unit_1.py
+++ service_1/testing/test_unit_1.py
@@ -1,9 +1,9 @@
 
 from flask import url_for
 from flask_testing import TestCase
-import requests_mock
-import app
-from application import app, db
+from requests_mock import mock
+from app import app, db
+from application.models import history
 
 class TestBase(TestCase):
     def create_app(self):
@@ -14,23 +14,27 @@ class TestBase(TestCase):
         )
         return app
 
-    def setup(self):
+    def setUp(self):
         db.create_all()
+        test = history(rarity='Pink', gun = 'AWP', price = 9)
+        db.session.add(test)
+        db.session.commit()
 
-    def teardown(self):
+
+    def tearDown(self):
         db.drop_all()
 
 class TestResponse(TestBase):
     def test_index(self):
-        with requests_mock.mock() as m:
-            a = self.client.get(url_for('index'))
-            self.assert500(a)
+        with mock() as m:
+            # a = self.client.get(url_for('index'))
+            # self.assert500(a)
             m.get('http://service_2:5002/get/rarity', json='Blue')
             m.get('http://service_3:5003/get/gun', json='Glock-18')
             m.post('http://service_4:5004/post/winnings', json=2)
 
             response = self.client.get(url_for('index'))
 
-        self.assert500(response)
+        self.assert200(response)
         self.assertIn('You rolled a Blue Glock-18 worth £2', response.data.decode())
         
