5587c1d31ac24766cc57119a88146aed3f765160
Pranay Wara <pranay.wara@gmail.com>
2021-08-11 08:43:46 +0000

    pw:deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,9 @@
 
 
 # copy over compose yaml to manager 
-scp -i ~/.ssh/id_rsa.pub docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
+scp ~/Practical-Project/docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 
 # docker stack deploy
 ssh -i ~/.ssh/id_rsa.pub jenkins@swarm-manager << EOF
-    export DATABASE_URI=${DATABASE_URI}
-    export SECRET_KEY=${SECRET_KEY}
-    docker stack deploy --compose-file docker-compose.yaml draft
+    docker stack deploy --compose-file docker-compose.yaml skin-opener
 EOF
