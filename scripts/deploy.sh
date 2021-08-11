72a3fa522dddcfad53925f11134644980b50d1b3
Pranay Wara <pranay.wara@gmail.com>
2021-08-11 10:20:12 +0000

    pw:scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,9 @@
 
 
 # copy over compose yaml to manager 
-scp ~/.ssh/id_rsa.pub docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
+scp ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 
 # docker stack deploy
-ssh -i ~/.ssh/id_rsa.pub jenkins@swarm-manager << EOF
+ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
     docker stack deploy --compose-file docker-compose.yaml skin-opener
 EOF
